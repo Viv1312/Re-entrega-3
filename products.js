11e5ff03949dc823a9f52e65747f230ae4387962
Viv1312 <86809851+Viv1312@users.noreply.github.com>
2022-12-06 17:19:09 -0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+      id: 1,
+      nombre:"Galletas de Avena",
+      precio: 110,
+      img: 
+          "https://i.pinimg.com/564x/b1/4c/42/b14c425f6f6cbb654f3085b1b04ebcf4.jpg",
+      cantidad: 1, 
+  
+    },
+
+    {
+     id: 2,
+     nombre:"Jugo de Arandanos",
+     precio: 500,
+     img:
+          "https://i.pinimg.com/564x/0e/94/5d/0e945d93bd6492f51f1ce1f5c5f55f3d.jpg",  
+     cantidad: 1,      
+    },
+    {
+     id: 3, 
+     nombre:"Pistachos confitados",
+     precio: 900,
+     img:
+          "https://i.pinimg.com/564x/3e/6a/6a/3e6a6a673c45044b939e41d3828b6583.jpg",
+     cantidad: 1,     
+    },
+    {
+        id: 4,
+        nombre:"Leche de Almendras",
+        precio: 700,
+        img: 
+           "https://i.pinimg.com/564x/ce/94/42/ce944298ce424bef9a5f79fd069295aa.jpg",
+        cantidad: 1, 
+    },
+      
+  ];
